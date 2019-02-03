56ea75887fc9cb063774f474a896fc690673fce1
ibrahimcakmak <ibrcakmak@gmail.com>
2019-02-02 21:23:58 -0500

    the day after

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -37,7 +37,15 @@ public class LoginTests {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-        driver.findElement(By.id("ctl00_logout")).clear();
+        driver.findElement(By.id("ctl00_logout")).click();
+        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+    }
+    @Test
+    public void logOutTest1() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+        driver.findElement(By.id("ctl00_logout")).click();
         Assert.assertEquals(driver.getTitle(), "Web Orders Login");
     }
 
