99a2bd41ffc16ce7b23a41fd51c7f35aeadd7066
ibrahimcakmak <ibrcakmak@gmail.com>
2019-02-03 23:45:36 -0500

    commit4.add negative login test

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -49,6 +49,15 @@ public class LoginTests {
         Assert.assertEquals(driver.getTitle(), "Web Orders Login");
     }
 
+    @Test
+    public void logOutTest2() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester2");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test3" + Keys.ENTER);
+        driver.findElement(By.id("ctl00_logout")).click();
+        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+    }
+
     @AfterMethod
     public void cleanUp() {
         driver.close();
